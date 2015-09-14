7497e3d9354bcdd04a7d042891e35569805b9a7f
Nik Silver <nik@niksilver.com>
2015-09-14 17:13:42 +0100

    Removed some redundant code, and added some Scaladoc.

--- src/main/scala/org/pigsaw/familytree/FamilyTree.scala
+++ src/main/scala/org/pigsaw/familytree/FamilyTree.scala
@@ -6,10 +6,13 @@ package org.pigsaw.familytree
 class FamilyTree {
   self =>
 
+  /** Every `Person` in this family tree. */
   val people: Seq[Person] = Nil
 
+  /** Every parent-child pair in this family tree. */
   val parentChildPairs: Seq[(Person,Person)] = Nil
 
+  /** Add a person into this. */
   def +(p: Person): FamilyTree = {
     if (people contains p) {
       throw new Exception(s"Person $p already exists")
@@ -20,12 +23,10 @@ class FamilyTree {
     }
   }
 
+  /** Specify a parent-child relationship. */
   def parentOf(parent: Person, child: Person): FamilyTree = {
     if (!people.contains(parent) || !people.contains(child)) {
       throw new Exception(s"Parent $parent is not in the tree")
-    }
-    if (parentsOf(child).length == 2) {
-      throw new Exception("Child has enough parents already")
     }
     if (hasFather(child) && parent.sex == Male) {
       throw new Exception("Child already has a father")
@@ -39,17 +40,24 @@ class FamilyTree {
     }
   }
 
+  /** Specify a child-parent relationshiop. */
   def childOf(child: Person, parent: Person): FamilyTree = parentOf(parent, child)
 
+  /** All the parents. */
   def parents = (parentChildPairs map ( _._1 )).distinct
 
+  /** All the female parents. */
   def mothers = parents filter {_.sex == Female}
 
+  /** All the male parents. */
   def fathers = parents filter {_.sex == Male}
 
+  /** Get the parents of a child. */
   def parentsOf(child: Person) = (parentChildPairs filter (_._2 == child) map ( _._1 ))
 
+  /** Does the `child` have a father in this family tree? */
   def hasFather(child: Person) = parentsOf(child) exists { _.sex == Male }
 
+  /** Does the `child` have a mother in this family tree? */
   def hasMother(child: Person) = parentsOf(child) exists { _.sex == Female }
 }
